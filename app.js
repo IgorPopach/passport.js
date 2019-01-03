ef10b7c0a584ab88c9ca0cec95e1eeb0f77adc51
unknown <igorpopach@gmail.com>
2019-01-03 23:53:06 +0200

    passport-local

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/node-auth')
+mongoose.connect('mongodb://restaurant:restaurant1@ds139614.mlab.com:39614/restaurant')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
